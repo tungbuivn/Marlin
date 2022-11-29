c8a1cfdd9b22cc0ba5271313bde52db821bbd071
tung <tungbt@websosanh.org>
2022-11-29 11:36:52 +0700

    enable z step align

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -958,7 +958,7 @@
  * Z Steppers Auto-Alignment
  * Add the G34 command to align multiple Z steppers using a bed probe.
  */
-//#define Z_STEPPER_AUTO_ALIGN
+#define Z_STEPPER_AUTO_ALIGN
 #if ENABLED(Z_STEPPER_AUTO_ALIGN)
   /**
    * Define probe X and Y positions for Z1, Z2 [, Z3 [, Z4]]
@@ -966,7 +966,7 @@
    * If not defined, probe limits will be used.
    * Override with 'M422 S<index> X<pos> Y<pos>'.
    */
-  //#define Z_STEPPER_ALIGN_XY { {  10, 190 }, { 100,  10 }, { 190, 190 } }
+  #define Z_STEPPER_ALIGN_XY { {  280, 280 }, { 15,  280 }, { 15, 15 },{ 280, 15 } }
 
   /**
    * Orientation for the automatically-calculated probe positions.
